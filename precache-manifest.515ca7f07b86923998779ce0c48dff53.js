b76ba4b635929585ebb3b438e05886222da5b7aa
joel <joeldsz123@gmail.com>
2020-06-17 22:32:09 +0530

    Updates

---
+++ precache-manifest.515ca7f07b86923998779ce0c48dff53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf5338e80824edc3ba181fa7c39bfd6",
+    "url": "/super-jokes/index.html"
+  },
+  {
+    "revision": "0f2872e07ee7509c3588",
+    "url": "/super-jokes/static/css/main.015a44e5.chunk.css"
+  },
+  {
+    "revision": "6bebc9daf82393c48c7c",
+    "url": "/super-jokes/static/js/2.4da570c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/super-jokes/static/js/2.4da570c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2872e07ee7509c3588",
+    "url": "/super-jokes/static/js/main.5c175195.chunk.js"
+  },
+  {
+    "revision": "a65c11494234c5d6535f",
+    "url": "/super-jokes/static/js/runtime-main.e7ed1288.js"
+  }
+]);
